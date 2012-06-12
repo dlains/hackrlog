b924f71cc59f9d26b401e4226bdf30edfc0ced9a
David Lains <dlains@gmail.com>
2012-06-12 11:47:13 -0700

    Added nginx config to the site manifest. Added .vagrant to .gitignore.

--- ops/manifests/site.pp
+++ ops/manifests/site.pp
@@ -2,3 +2,31 @@ Exec
 {
   path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 }
+
+package
+{
+  "nginx": ensure => present
+}
+
+service
+{
+  "nginx": ensure => true, enable => true
+}
+
+file
+{
+  "/etc/nginx/nginx.conf":
+    source => "/etc/puppet/modules/nginx/nginx.conf",
+    mode   => 644,
+    owner  => root,
+    group  => root
+}
+
+file
+{
+  "/etc/nginx/conf.d/hackrlog.conf":
+    source => "/etc/puppet/modules/nginx/conf.d/hackrlog.conf",
+    mode   => 644,
+    owner  => root,
+    group  => root
+}
