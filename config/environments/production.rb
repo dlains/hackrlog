c395cc80db65e5362fb059456835f4c7abe0e559
David Lains <dlains@gmail.com>
2012-07-30 14:28:05 -0700

    More SMTP email configuration tweaking.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,15 +48,16 @@ Hackrlog::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
-  # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
-
   # Mail configuration
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'mail.hackrlog.com',
     user_name: 'support',
     password: 'khaym4nz0r',
-    authentication: :login
+    authentication: :login,
+    domain: 'hackrlog.com',
+    enable_starttls_auto: false
   }
   
   # Set the host for URLs inside mailers.
