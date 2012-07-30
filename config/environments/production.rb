4bf7d3e1fb9b7267da33535f5ff7a09fa620c40c
David Lains <dlains@gmail.com>
2012-07-30 13:59:29 -0700

    Added SMTP configuration for handling email deliveries.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,16 @@ Hackrlog::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  # Mail configuration
+  config.action_mailer.smtp_settings = {
+    user_name: 'support',
+    password: 'khaym4nz0r'
+    authentication: :login
+  }
+  
+  # Set the host for URLs inside mailers.
+  config.action_mailer.default_url_options = { host: 'hackrlog.com' }
+  
   # Enable threaded mode
   # config.threadsafe!
 
@@ -64,9 +74,4 @@ Hackrlog::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
-  # TODO: Double check the host value and make sure the URLs work.
-  # Set the host for URLs inside mailers.
-  config.action_mailer.default_url_options = { host: 'hackrlog.com' }
-  
 end
