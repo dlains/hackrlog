abfa281fdf52a3469e25581413fc5cdca50a582b
David Lains <dlains@gmail.com>
2012-07-30 14:18:12 -0700

    Fixed problem with SMTP configuration.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,6 +53,7 @@ Hackrlog::Application.configure do
 
   # Mail configuration
   config.action_mailer.smtp_settings = {
+    address: 'mail.hackrlog.com'
     user_name: 'support',
     password: 'khaym4nz0r',
     authentication: :login
