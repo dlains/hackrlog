b49c0c1676bcc9b60596c83eb2946eb20f58d5e5
David Lains <dlains@gmail.com>
2012-07-31 10:05:27 -0700

    More SMTP config settings. Will this ever work?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,8 +52,13 @@ Hackrlog::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'mail.hackrlog.com',
-    enable_starttls_auto: false
+    address:        'mail.hackrlog.com',
+    port:           587,
+    domain:         'mail.hackrlog.com',
+    authentication: 'plain',
+    user_name:      'support',
+    password:       'khaym4nz0r',
+    enable_starttls_auto: true
   }
   
   # Set the host for URLs inside mailers.
